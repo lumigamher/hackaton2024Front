3b0767079a6ba68ef78c4a3e9edff5cc1d14c8ea
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-09-21 12:16:23 -0500

    Feat: Login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import Input from '../components/ui/input';
 import Button from '../components/ui/Button'
+import { Link } from 'react-router-dom';
+
 
 function LoginPage() {
   const { login } = useAuth()
@@ -9,6 +11,7 @@ function LoginPage() {
     username: '',
     password: ''
   });
+  const [isVisible, setisVisible] = useState(false)
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,28 +28,66 @@ function LoginPage() {
 
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <Input
-          placeholder='User'
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
+  const toogleVisibility = () => {
+    setisVisible(prev => !prev)
+  }
 
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder='Password'
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <Button type='submit'> Login </Button>
-      </form>
+  return (
+
+    <div className="flex h-screen w-screen justify-between bg-gray-100">
+
+      <div className="w-6 bg-white">
+        <h2 className="translate-y-7 rotate-90 font-bold">ChroniX</h2>
+      </div>
+
+      <div className=' text-center grid grid-rows-3 grid-cols-3'>
+        <form onSubmit={handleSubmit}
+          className='row-start-2 row-end-3 col-start-2 flex flex-col  gap-1'>
+          <h2 className='text-6xl text-center font-bold mb-5'> Login </h2>
+          <Input
+            placeholder='User'
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+
+          />
+          <div>
+
+            <i
+              className={`absolute translate-x-5 translate-y-7 py-0.5 hover:cursor-pointer bx-low-vision  bx ${isVisible ? 'text-gray-200' : 'text-black'}`}
+              onClick={toogleVisibility}
+            >
+            </i>
+
+            <Input
+              type={isVisible? 'text' : 'password'}
+              name="password"
+              placeholder='Password'
+
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <i
+              className='absolute -translate-x-8 translate-y-7 pt-0.5 hover:cursor-pointer text-gray-400 bx bx-right-arrow-alt'
+              onClick={handleSubmit}
+            >
+
+            </i>
+          </div>
+          <Button> <i className='bx bxl-google'></i> Sign In With Google </Button>
+        </form>
+
+        <div className='row-start-3 row-end-4 col-start-2 self-end pb-10 flex flex-col gap-5' >
+          <p className='text-xl font-normal'> Don't have an account? <Link to='/register' className='text-orange-400 underline'>Sign up here</Link> </p>
+          <p className='text-1xl font-noraml  text-gray-800 hover:cursor-pointer'> Terms of Use | Privacy Policy</p>
+        </div>
+      </div>
+
+      <div className="w-6 bg-white" />
+
     </div>
   );
 }
