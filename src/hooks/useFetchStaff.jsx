edf8f3703f908544abc6cd6f95067b927c8b7c18
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-09-23 13:20:35 -0500

    Staff service y useFetchStaff creados

---
+++ src/hooks/useFetchStaff.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import { staffService } from '../services/staffService';
+
+const useFetchStaff = (projectId = null) => {
+    const [staff, setStaff] = useState([]);  
+    const [loading, setLoading] = useState(true);  
+    const [error, setError] = useState(null); 
+    const fetchStaff = async () => {
+        setLoading(true);
+        setError(null); 
+
+        try {
+            const data = projectId 
+                ? await staffService.getStaffByProject(projectId) 
+                : await staffService.getAllAvailStaff();
+
+            setStaff(data); 
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false); 
+        }
+    };
+
+    useEffect(() => {
+        fetchStaff(); 
+    }, [projectId]);
+
+    return { staff, loading, error, refetch: fetchStaff };
+};
+
+export default useFetchStaff;
---
+++ src/services/staffService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const staffService = {
+    getAllAvailStaff: async () => {
+        try {
+            const response = await axios.get('/usuarios/sin-proyectos');
+            return response.data; 
+        } catch (error) {
+            console.warn("Error al obtener personal disponible: ", error);
+            throw error; 
+        }
+    },
+    getStaffByProject: async (id) => { 
+        try {
+            const response = await axios.get(`proyectos/${id}`);
+            return response.data; 
+        } catch (error) {
+            console.warn("Error al obtener personal por proyecto: ", error);
+            throw error; 
+        }
+    }
+};
