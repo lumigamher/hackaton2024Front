05bc11bc33f2767bb62bc73176d87da2e8e4c3ae
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-09-23 22:46:36 -0500

    "Updated axios instance configuration: changed testURL and testURL2 from https to http, removed httpsAgent configuration"

--- src/api/axiosInstace.js
+++ src/api/axiosInstace.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 import https from 'https'
 
-const testURL = 'https://54.147.236.208'
-const testURL2 = 'https://54.147.236.208'
+const testURL = 'http://54.147.236.208'
+const testURL2 = 'http://54.147.236.208'
 const local = 'http://localhost:8080'
 
 const instance = axios.create({
     baseURL: testURL2,
-    httpsAgent: new https.Agent({
-        rejectUnauthorized: false,
-    })
 })
 
 
