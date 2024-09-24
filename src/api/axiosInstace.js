9d93e372749d48dc769f0e637d9ee108267d4eb6
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-09-23 23:06:39 -0500

    "Updated testURL and testURL2 from http to https"

--- src/api/axiosInstace.js
+++ src/api/axiosInstace.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import https from 'https'
 
-const testURL = 'http://54.147.236.208'
-const testURL2 = 'http://54.147.236.208'
+const testURL = 'https://54.147.236.208'
+const testURL2 = 'https://54.147.236.208'
 const local = 'http://localhost:8080'
 
 
