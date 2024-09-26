a15fdea9710a612090716ff68f4460aff21f6bc8
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-09-26 00:48:34 -0500

    Intentar subir commit.

--- src/api/awsConfig.tsx
+++ src/api/awsConfig.tsx
@@ -1,1 +1,12 @@
 
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+const s3 = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: 'AKIA2CUNLQGQFFLBH3GL',
+        secretAccessKey: "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"
+    }
+});
+
+export default s3;
